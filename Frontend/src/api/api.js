17783b30a680376d5335b4b3f988754a9e88b3c3
deepankar-Sharma92 <deepankarbenchmate@gmail.com>
2026-03-20 14:44:05 +0530

    Update api.js

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://djano-reactjs.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
